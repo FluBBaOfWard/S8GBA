9f5d6906f3f02c4054926f5df2fefaf62c8b9c2c
Fredrik Ahlström <fredrikahlstrom75@gmail.com>
2026-07-06 15:16:41 +0200

    Optimised sprite conversion.

--- source/Gui.c
+++ source/Gui.c
@@ -16,7 +16,7 @@
 #include "SegaVDP/Version.h"
 #include "SN76496/Version.h"
 
-#define EMUVERSION "V1.1.8 2026-07-02"
+#define EMUVERSION "V1.1.8 2026-07-06"
 
 static void gammaChange(void);
 static void selectMachine(void);

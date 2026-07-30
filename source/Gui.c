3cbb32532b85aad29fa9b07d52b26f801690ca93
Fredrik Ahlström <fredrikahlstrom75@gmail.com>
2026-07-30 13:02:35 +0200

    Some tweaks before release.

--- source/Gui.c
+++ source/Gui.c
@@ -16,7 +16,7 @@
 #include "SegaVDP/Version.h"
 #include "SN76496/Version.h"
 
-#define EMUVERSION "V1.1.8 2026-07-29"
+#define EMUVERSION "V1.1.8 2026-07-30"
 
 static void gammaChange(void);
 static void selectMachine(void);
